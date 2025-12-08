ef7e8bbaaea9724dbddd8c3394074fe7ef07972c
Kilian Koeltzsch <me@kilian.io>
2025-12-08 21:59:25 +0100

    default to german tts

--- congress/static/js/common.js
+++ congress/static/js/common.js
@@ -489,6 +489,7 @@ class TTSManager {
     this.stop();
 
     const utterance = new SpeechSynthesisUtterance(text);
+    utterance.lang = options.lang || "de-DE";
     utterance.rate = options.rate || 1.0;
     utterance.pitch = options.pitch || 1.0;
     utterance.volume = options.volume || 1.0;
