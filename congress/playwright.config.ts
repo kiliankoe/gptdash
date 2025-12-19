58a79168cc47ea96d83d71204dc8a323da916915
Kilian Koeltzsch <me@kilian.io>
2025-12-19 17:12:28 +0100

    allow running e2e tests against other browsers, also mobile

--- congress/playwright.config.ts
+++ congress/playwright.config.ts
@@ -49,6 +49,24 @@ export default defineConfig({
       name: "chromium",
       use: { ...devices["Desktop Chrome"] },
     },
+    {
+      name: "firefox",
+      use: { ...devices["Desktop Firefox"] },
+    },
+    {
+      name: "webkit",
+      use: { ...devices["Desktop Safari"] },
+    },
+    {
+      name: "mobile-chrome",
+      use: { ...devices["Pixel 5"] },
+      testMatch: ["audience.spec.ts", "player.spec.ts"],
+    },
+    {
+      name: "mobile-safari",
+      use: { ...devices["iPhone 12"] },
+      testMatch: ["audience.spec.ts", "player.spec.ts"],
+    },
   ],
 
   webServer: {
