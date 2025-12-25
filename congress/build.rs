cb6e6b6bd49c685a86401a2b756621ea65a006f7
Kilian Koeltzsch <me@kilian.io>
2025-12-25 09:56:47 +0100

    run npm install during build

--- congress/build.rs
+++ congress/build.rs
@@ -1,11 +1,18 @@
 fn main() {
     println!("cargo:rerun-if-changed=static/js/");
     println!("cargo:rerun-if-changed=scripts/build-frontend.mjs");
+    println!("cargo:rerun-if-changed=package.json");
+    println!("cargo:rerun-if-changed=package-lock.json");
+
+    let status = std::process::Command::new("npm")
+        .args(["ci", "--ignore-scripts"])
+        .status()
+        .expect("Failed to run npm ci - is npm installed?");
+    assert!(status.success(), "npm ci failed");
 
     let status = std::process::Command::new("npm")
         .args(["run", "build:frontend"])
         .status()
-        .expect("Failed to run frontend build - is npm installed?");
-
+        .expect("Failed to run frontend build");
     assert!(status.success(), "Frontend build failed");
 }
