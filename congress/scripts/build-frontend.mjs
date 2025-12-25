ad336d797f5b50c0971aa9ceef38f54b8de11d0f
Kilian Koeltzsch <me@kilian.io>
2025-12-25 13:40:13 +0100

    disable string array obfuscation

--- congress/scripts/build-frontend.mjs
+++ congress/scripts/build-frontend.mjs
@@ -22,9 +22,7 @@ const obfuscatorOptions = {
 	controlFlowFlattening: true,
 	controlFlowFlatteningThreshold: 0.5,
 	identifierNamesGenerator: "mangled-shuffled",
-	stringArray: true,
-	stringArrayEncoding: ["base64"],
-	stringArrayThreshold: 0.5,
+	stringArray: false,
 	renameGlobals: false,
 };
 
